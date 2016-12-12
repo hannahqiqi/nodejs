92396b9b8d4b2a1209fe3df3526e94c4dcca90af
hannahyu <748249415@qq.com>
2016-12-12 09:51:16 +0800

    add pages&css

--- node/begin/server.js
+++ node/begin/server.js
@@ -1,12 +1,18 @@
 var http = require("http");
 var fs = require("fs");
+var url = require("url");
 
 http.createServer(function (request, response) {
 	
-	fs.readFile("./public/hello.html", "utf8", function(err, data) {
-		response.writeHead(200, {"Content-Type": "text/html", "Access-Control-Allow-Origin": "*"});
-		response.write(data);
-		response.end();
+	var fullPath = url.parse(request.url, true);
+	var pathArr = fullPath.pathname.split(".");
+	
+	fs.readFile("./public" + fullPath.pathname, "utf8", function(err, data) {
+		if (err) {
+			response.writeHead(404, {"Content-Type": "text/plain"});
+			response.end("The page you found does not exist.");
+		}
+		response.end(data);	
 	});
 	
-}).listen(3000);
+}).listen(3003);
