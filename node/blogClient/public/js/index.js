a37cc128fd56083c29dd4e3877b903e88c55e3d6
hannahyu <748249415@qq.com>
2017-01-06 21:08:15 +0800

    add update function

--- node/blogClient/public/js/index.js
+++ node/blogClient/public/js/index.js
@@ -19,7 +19,8 @@ function loadPost() {
     var html = ""
     for (var item of data) {
       var localTime = moment(item.postTime).format('YYYY-MM-DD HH:mm')
-      html += `<li>${item.post}&nbsp;${localTime} <button type="button" class="btn btn-default" style="border:none" onclick="delPost('${item._id}')">删除</button></li>`
+      html += `<li>${item.post}&nbsp;${localTime} <button type="button" class="btn btn-default" style="border:none" onclick="delPost('${item._id}')">删除</button>
+      <button type="button" class="btn btn-default" style="border:none" onclick="editPost('${item._id}', '${item.post}')">编辑</button></li>`
     }
     $("#blogs").html(html)
   })
@@ -39,8 +40,25 @@ function searchPost() {
     var html = ""
     for (var item of data) {
       var localTime = moment(item.postTime).format('YYYY-MM-DD HH:mm')
-      html +=  `<li>${item.post}&nbsp;${localTime} <button type="button" class="btn btn-default" style="border:none" onclick="delPost('${item._id}')">删除</button></li>`
+      html += `<li>${item.post}&nbsp;${localTime} <button type="button" class="btn btn-default" style="border:none" onclick="delPost('${item._id}')">删除</button></li>`
       $("#blogs").html(html)
     }
+  })
+}
+
+function editPost(postId, content) {
+  $('#newid').val(postId)
+  $('#newcontent').val(content)
+  $('#myModal').modal('show')
+}
+
+function updatePost() {
+  var id = $('#newid').val()
+  var content = $('#newcontent').val()
+  $.get("http://localhost:2000/updateblog", { id: id, dta: content }, function(data) {
+    // alert(id)
+    // alert(content)
+    loadPost()
+    $('#myModal').modal('hide')
   })
 }
--- node/blogServer/app.js
+++ node/blogServer/app.js
@@ -13,8 +13,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const url = 'mongodb://achen:th123456@ds151028.mlab.com:51028/aaronchlab'
 //const url = "mongodb://192.168.1.100:27017/blog"
 
-app.get('/blogs', function(req, res) {
-    MongoClient.connect(url, function(err, db) {
+app.get('/blogs', function (req, res) {
+    MongoClient.connect(url, function (err, db) {
         var collection = db.collection('post')
         collection.find().toArray(function (err, docs) {
             db.close()
@@ -23,36 +23,52 @@ app.get('/blogs', function(req, res) {
     })
 })
 
-app.post('/blog', function(req, res) {
+app.post('/blog', function (req, res) {
     var post = req.body.post
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
         var collection = db.collection('post')
-        collection.insert({post:post, postTime:new Date()}, function() {
+        collection.insert({ post: post, postTime: new Date() }, function () {
             db.close()
             res.send('ok')
         })
     })
 })
 
-app.get('/delblog', function(req, res) {
+app.get('/delblog', function (req, res) {
     var postId = req.query.id
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
         var collection = db.collection('post')
-        collection.remove({"_id":ObjectId(postId)})
+        collection.remove({ "_id": ObjectId(postId) })
         db.close()
         res.send('ok')
     })
 })
 
-app.get('/searchblog', function(req, res) {
+app.get('/searchblog', function (req, res) {
     var content = req.query.dta //dta--前端ajax传过去的key是什么 xxx就是什么; get过去的就是query; post过去的就是body
     console.log(content)
-    MongoClient.connect(url, function(err, db) {
+    MongoClient.connect(url, function (err, db) {
         var collection = db.collection('post')
         var re = new RegExp(content)//MySQL的%m%(mongodb nodejs like)
-        collection.find({"post": re}).toArray(function (err, docs) {
+        collection.find({ "post": re }).toArray(function (err, docs) {
             db.close()
             res.send(docs)
+        })
+    })
+})
+
+app.get('/updateblog', function (req, res) {
+    var postId = req.query.id
+    var content = req.query.dta
+    console.log(content)
+    MongoClient.connect(url, function (err, db) {
+        console.log("connected")
+        var collection = db.collection('post')
+        collection.remove({ _id: ObjectId(postId) }, function (err, result) {
+            collection.insert({ _id: ObjectId(postId), post: content, postTime: new Date() }, function (err, result) {
+                db.close()
+                res.send('ok')
+            })
         })
     })
 })
